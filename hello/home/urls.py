86200eae59ab85930bb8680fe951a73394346c89
jahnvigangwar <jahnvigangwar@gmail.com>
2023-06-09 09:49:28 +0530

    first commit

---
+++ hello/home/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from home import views
+
+admin.site.site_header = "Jahnvi's Django APP Admin"
+admin.site.site_title = "Jahnvi's Django APP Admin Portal"
+admin.site.index_title = "Welcome to Jahnvi's Django APP Portal"
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
+]
---
+++ hello/home/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def index(request):
+    context = {
+        "variable" : "sending variable"
+    }
+    return render(request, 'index.html', context)
+    # return HttpResponse('this is hime page')
+
+def about(request):
+    return render(request, 'about.html')
+    # return HttpResponse('this is ABOUT page')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+    # return HttpResponse('this is CONTACT page')
