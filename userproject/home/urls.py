7fad04cef5fa8af7f7bfaec5fdd2f1380c578673
jahnvigangwar <jahnvigangwar@gmail.com>
2023-06-09 13:07:16 +0530

    login page

---
+++ userproject/home/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+from home import views
+
+admin.site.site_header = "Jahnvi's Django APP Admin"
+admin.site.site_title = "Jahnvi's Django APP Admin Portal"
+admin.site.index_title = "Welcome to Jahnvi's Django APP Portal"
+
+urlpatterns = [
+    
+    path('', views.index, name='index'),
+    path('login', views.loginUser, name='loginUser'),
+    path('logoutUser', views.logoutUser, name='logoutUser'),
+
+]
